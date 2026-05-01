048a469a7bd3cecc5267a25dfce97c42570d8adf
unknown <luisfelipe.fsk@gmail.com>
2026-05-01 04:37:05 -0300

    feat(): h2 03 & 04

--- 02_sequential_basics/02_04_serial_adder_with_vld/02_04_serial_adder_with_vld.sv
+++ 02_sequential_basics/02_04_serial_adder_with_vld/02_04_serial_adder_with_vld.sv
@@ -28,6 +28,18 @@ module serial_adder_with_vld
   // only if vld is also high, otherwise last should be ignored.
   //
   // When rst is high, the module should reset its internal state.
+  
+  logic carry;
+  wire carry_comb;
+  assign {carry_comb, sum} = a + b + carry;
 
+  always_ff @(posedge clk)
+    if (rst) begin
+      carry <= '0;
+    end else if (vld && last) begin
+      carry <= '0;
+    end else if (vld) begin
+      carry <= carry_comb;
+    end
 
 endmodule
